050c54bdd6f41fd2403d21e549dab100af0537ed
LeLapinouAlpha <t-henon@hotmail.fr>
2024-05-31 20:22:34 +0200

    feat(volume): implement 'volume_create' func

--- src/volume/volume.c
+++ src/volume/volume.c
@@ -1,6 +1,19 @@
 #include "volume.h"
 
 #include <stdlib.h>
+#include <string.h>
+
+struct volume *volume_create(const char *version, size_t capacity)
+{
+    struct volume *vol = malloc(sizeof(struct volume));
+    if (vol != NULL)
+    {
+        strncpy(vol->version, version, VOLUME_VERSION_SIZE);
+        vol->capacity = capacity;
+        vol->root = file_create("/", 0, 0, "me", true);
+    }
+    return vol;
+}
 
 void volume_free(struct volume *vol)
 {
